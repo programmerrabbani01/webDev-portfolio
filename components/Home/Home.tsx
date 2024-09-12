858ceaf043eab001bc8812167c0adf42024315e3
programmerrabbani01 <programmerrabbani@gmail.com>
2024-09-12 20:51:15 +0600

    Services Section Done

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./Hero/Hero.tsx";
 import About from "./About/About.tsx";
+import Services from "./Services/Services.tsx";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="overflow-hidden">
         <Hero />
         <About />
+        <Services />
       </div>
     </>
   );
---
+++ components/Home/Services/Services.tsx
@@ -0,0 +1,26 @@
+import SectionHeading from "@/components/Helper/SectionHeading.tsx";
+import { servicesData } from "@/Data/data.ts";
+import React from "react";
+import ServiceCard from "./ServiceCard.tsx";
+
+export default function Services() {
+  return (
+    <>
+      <div className="pt-16 pb-16 bg-[#0f0715]">
+        {/* section Heading */}
+        <SectionHeading>Services</SectionHeading>
+        {/* content */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-4/5 mx-auto items-center mt-20">
+          {servicesData.map((service) => {
+            return (
+              <div key={service.id} className="">
+                {/* service card */}
+                <ServiceCard service={service} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
